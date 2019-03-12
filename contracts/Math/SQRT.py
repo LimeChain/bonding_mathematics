45eab2f61a6a368bcb99df3e8a4df3ec7257dd38
Lyubomir Kiprov <lyubo@limechain.tech>
2019-03-12 18:51:52 +0200

    Add sqrt_hfp

--- contracts/Math/SQRT.py
+++ contracts/Math/SQRT.py
@@ -1,30 +1,5 @@
 @public
 @constant
-def token_sqrt(num: uint256) -> uint256:
-    if num == 0:
-        return 0
-
-    normalization: decimal = 1000000000000000000.0
-    normalizedNumber: decimal = convert(num, decimal) / normalization
-
-    assert normalizedNumber >= 1.0
-
-    root: decimal = normalizedNumber
-    rootCalculations: decimal = (normalizedNumber + 1.0) / 2.0
-
-    for i in range(256):
-        if (rootCalculations >= root):
-            break
-
-        root = rootCalculations
-        rootCalculations = (normalizedNumber / root + root) / 2.0
-
-    root *= normalization
-
-    return convert(root, uint256)
-
-@public
-@constant
 def sqrt(num: decimal) -> uint256:
     if num == 0.0:
         return 0
